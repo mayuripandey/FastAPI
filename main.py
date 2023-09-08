4fd022659d6a2e407c83e831c3cdbb9e5bd412ee
Mayuri Pandey <mayuripandey4@gmail.com>
2023-09-08 09:07:03 +0200

    Changes in route

--- main.py
+++ main.py
@@ -8,5 +8,14 @@ def index():
 
 
 @app.get('/about')
-def index():
+def about():
     return {'data':{'about page test'}}
+
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 'all unpublished blogs'}
+    
+@app.get('/blog/{id}')
+def show(id : int):
+    return {'data': id}
