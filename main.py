dc7a79ad5766d2c48895ad44c4da96309e6255e2
Mayuri Pandey <mayuripandey4@gmail.com>
2023-09-08 15:52:19 +0200

    Crud routes

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -13,6 +15,15 @@ def about():
     return {'data':{'about page test'}}
 
 
+@app.get('/blog')
+def index(limit=10, published: bool = True, sort: Optional[str] = None):
+    if published:
+    #list al blogs
+        return {'data': f'{limit} blog from db'}
+    else:
+        return {'data': f'{limit} blogs from the db'}
+
+
 @app.get('/blog/unpublished')
 def unpublished():
     return {'data': 'all unpublished blogs'}
@@ -20,3 +31,14 @@ def unpublished():
 @app.get('/blog/{id}')
 def show(id : int):
     return {'data': id}
+
+#posts
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+@app.post('/blog')
+def create_post(request: Blog):
+    return request
+    return {'data': 'post created'}
